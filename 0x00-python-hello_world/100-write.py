c1d71a91c0da3d032a84e03f009d25794475048a
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-09 14:27:40 -0700

    Using stderr.write instead of os.sys.write in 0x00 task 11

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,6 +1,4 @@
 #!/usr/bin/python3
-import os
-os.write(os.sys.stderr.fileno(),
-    bytes("and that piece of art is useful - Dora Korpar, 2015-10-19\n",
-    encoding="utf-8"))
-os.sys.exit(1)
+import sys
+sys.stderr.write("and that piece of art is useful - Dora Korpar, 2015-10-19\n")
+sys.exit(1)
