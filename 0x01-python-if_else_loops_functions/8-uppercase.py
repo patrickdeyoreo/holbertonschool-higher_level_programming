a25277e0900dc15143c8354bdd40211514787dfc
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-10 22:30:49 -0700

    Attempting 0x01 task 5

--- 0x01-python-if_else_loops_functions/8-uppercase.py
+++ 0x01-python-if_else_loops_functions/8-uppercase.py
@@ -5,8 +5,8 @@ def uppercase(str):
     """ Prints a string in uppercase (followed by a newline) """
     while str:
         if ord('a') <= ord(str[0]) <= ord('z'):
-            print('{:c}'.format(ord(str[0]) & ~0x20), end="")
+            print('{:c}'.format(ord(str[0]) - ord('a') + ord('A')), end="")
         else:
-            print(str[0], end="")
+            print('{:c}'.format(ord(str[0])), end="")
         str = str[1:]
     print()
