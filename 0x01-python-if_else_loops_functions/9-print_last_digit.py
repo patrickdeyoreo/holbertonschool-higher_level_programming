424f8867f1702b230610a637c6faf267afd2cefe
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-10 20:25:56 -0700

    Handling non-integer values in 0x00 task 9

--- 0x01-python-if_else_loops_functions/9-print_last_digit.py
+++ 0x01-python-if_else_loops_functions/9-print_last_digit.py
@@ -2,7 +2,12 @@
 
 
 def print_last_digit(number):
-    """ Prints and returns the last digit of a number """
+    """
+    Prints and returns the last digit of a number.
+    If given something that's not a number, this returns None.
+    """
+    if not isinstance(number, int):
+        return (None)
     number = str(number)[-1]
     print(number, end="")
     return (int(number))
