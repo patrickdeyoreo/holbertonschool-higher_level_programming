f195370c2df87a8181175fbcef0d62049a6eda17
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-10 23:02:17 -0700

    Implemented 0x01 task 6 using same technique as 4 and 5

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
-print(", ".join([", ".join([
-    str(tens)+str(ones) for ones in range(tens + 1, 10)]) for tens in range(9)
-]))
+print(*(', '.join(
+    "{}{}".format(tens, ones) for ones in range(tens+1, 10)
+    ) for tens in range(9)), sep=', ')
