d9e94b1a78a4f7dcbf1670562e9b0edcc80c3eee
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-10 19:31:53 -0700

    0x00 task 3 looks wiillld but I'll take it as a lesson of what not to do

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
-for i in list(range(
-    ord('a'), ord('e')
-    )) + list(range(
-    ord('f'), ord('q')
-    )) + list(range(
-    ord('r'), ord('z') + 1
-    )):
+for i in list(
+        range(ord('a'), ord('e'))
+        ) + list(
+                range(ord('f'), ord('q'))
+                ) + list(
+                        range(ord('r'), ord('z') + 1)
+                        ):
     print('{:c}'.format(i), end="")
