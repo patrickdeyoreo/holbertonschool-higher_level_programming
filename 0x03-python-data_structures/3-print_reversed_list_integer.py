55418215bbf193c19ab7210f583846c8a2e1f7ab
Patrick DeYoreo <29614644+patrickdeyoreo@users.noreply.github.com>
2019-09-18 10:23:17 -0700

    Using join instead of unpacking with 'sep=' for 0x03 task 3

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -3,4 +3,4 @@
 
 def print_reversed_list_integer(my_list=[]):
     if my_list is not None:
-        print(*['{:d}'.format(n) for n in my_list[::-1]], sep='\n')
+        print('\n'.join(['{:d}'.format(n) for n in my_list[::-1]]))
