9f1bdbf22a0a316a910a3ec59af621075d87444a
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-17 23:19:20 -0700

    Trying 0x03 task 1 using loop rather than '\n'.join

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,5 @@
 
 def print_list_integer(my_list=[]):
     if my_list is not None:
-        print('\n'.join(['{}'.format(n) for n in my_list]))
+        for n in my_list:
+            print('{}'.format(n))
