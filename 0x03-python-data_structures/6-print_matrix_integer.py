c4a6346fe3e05cb6d291727618b44a931ee655d2
Patrick DeYoreo <29614644+patrickdeyoreo@users.noreply.github.com>
2019-09-18 10:35:42 -0700

    Checking if matrix is None in 0x03 task 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,4 +2,5 @@
 
 
 def print_matrix_integer(matrix=[[]]):
-    print('\n'.join([' '.join(['{:d}'.format(n) for n in a]) for a in matrix]))
+    if matrix is not None:
+        print('\n'.join([' '.join(['{:d}'.format(n) for n in a]) for a in matrix]))
