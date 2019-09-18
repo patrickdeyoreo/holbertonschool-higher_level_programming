4e643b0b947cffe5773fe38e550881d385a287bd
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-18 00:15:11 -0700

    Adding attempt at 0x03 task 13 (palindromes)

--- 0x03-python-data_structures/13-is_palindrome.c
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -1,12 +1,41 @@
 #include "lists.h"
 
 /**
- * is_palindome - determine if a singly linked list represents a palindrome
+ * _is_palindome - determine if a singly linked list is a palindrome (helper)
  * @head: a pointer to a pointer to the start of the list
  *
- * Return: 1 if the list represents a palindrome, otherwise 0
+ * Return: 1 if the list is a palindrome, otherwise 0
+ */
+int _is_palindrome(listint_t **head, listint_t *tail)
+{
+	int status;
+
+	if (!tail)
+		return (1);
+
+	status = _is_palindrome(head, tail->next);
+
+	if (tail == *head || tail->next == *head)
+	{
+		*head = tail;
+		return (status);
+	}
+	else if (status && (*head)->n == tail->n)
+	{
+		*head = (*head)->next;
+		return (1);
+	}
+	return (0);
+}
+
+
+/**
+ * is_palindome - determine if a singly linked list is a palindrome
+ * @head: a pointer to a pointer to the start of the list
+ *
+ * Return: 1 if the list is a palindrome, otherwise 0
  */
 int is_palindrome(listint_t **head)
 {
-	return (0);
+	return (head && _is_palindrome(head, *head));
 }
