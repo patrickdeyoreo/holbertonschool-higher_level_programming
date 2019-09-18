fc6792e7d077df66d62d7d8f65da1d7a3f9637b2
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-17 22:49:49 -0700

    Replaced 0x03 task 1 map solution with much simpler list expansion

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -3,4 +3,4 @@
 
 def print_list_integer(my_list=[]):
     if my_list is not None:
-        list(map(lambda n: print('{}'.format(n)), my_list))
+        print(*['{}'.format(n) for n in my_list], sep='\n')
