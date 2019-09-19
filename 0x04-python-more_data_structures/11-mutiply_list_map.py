7b9be809f9ce80f99f0c451cea0fd68873b5749e
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-19 12:31:53 -0700

    Shortened 0x04 task 11 to 3-line limit

--- 0x04-python-more_data_structures/11-mutiply_list_map.py
+++ 0x04-python-more_data_structures/11-mutiply_list_map.py
@@ -1,6 +1,3 @@
 #!/usr/bin/python3
-
-
 def mutiply_list_map(my_list=[], number=0):
-    """ Create a list of the product each elements and the given number  """
     return None if list is None else list(map(lambda x: x * number, my_list))
