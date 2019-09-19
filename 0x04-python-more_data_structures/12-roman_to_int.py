7059067a0e76d06a37d5c0a585b774893288e03a
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-19 16:51:33 -0700

    Adding first attempt at 0x04 task 12

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,1 +1,23 @@
 #!/usr/bin/python3
+
+from functools import reduce
+
+
+def roman_to_int(roman_string):
+    """ Convert Roman numerals to integers
+    """
+    n2n = {
+        'I': 1,
+        'V': 5,
+        'X': 10,
+        'L': 50,
+        'C': 100,
+        'D': 500,
+    }
+    try:
+        return reduce(
+            lambda l, r: r - l if l < r  else r + l,
+            map(n2n.get, roman_string.upper())
+        )
+    except (KeyError, TypeError):
+        return 0
