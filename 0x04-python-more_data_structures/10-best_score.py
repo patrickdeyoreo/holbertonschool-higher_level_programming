281e45a8fb1fdda2666a668230f7f99d374f1ed2
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-19 12:30:24 -0700

    Removed useless 'default' arg from max() & modified to only return key in 0x04 task 10

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -5,7 +5,5 @@ def best_score(a_dictionary):
     """ Retrieve the a key of the largest integer value
     """
     if a_dictionary:
-        return max(a_dictionary.items(),
-                   key=lambda pair: pair[1],
-                   default=None)
+        return max(a_dictionary.items(), key=lambda pair: pair[1])[:1]
     return None
