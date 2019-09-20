9d52688dcfc99b75711492f4d7463a7e822312fe
patrickdeyoreo <pdeyoreo@gmail.com>
2019-09-19 22:39:35 -0700

    Shortened 0x04 task 14 to 3 lines

--- 0x04-python-more_data_structures/101-square_matrix_map.py
+++ 0x04-python-more_data_structures/101-square_matrix_map.py
@@ -1,14 +1,3 @@
 #!/usr/bin/python
-
-
 def square_matrix_map(matrix=[]):
-    """ Create a matrix a of the square of each element
-    """
-    if matrix is not None:
-        return list(map(
-            lambda row: list(map(
-                lambda x: x ** 2, row
-            )),
-            matrix
-        ))
-    return None
+    return list(map(lambda row: list(map(lambda x: x ** 2, row)), matrix))
