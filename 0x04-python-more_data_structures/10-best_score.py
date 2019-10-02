5f4568966403561f690a8f1960efaf87c5a14592
Patrick DeYoreo <29614644+patrickdeyoreo@users.noreply.github.com>
2019-10-02 12:55:17 -0700

    Adjusted hanging indent

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -7,5 +7,5 @@ def best_score(a_dictionary):
     if a_dictionary is not None:
         return max(
             a_dictionary.items(), key=lambda item: item[1], default=[None]
-            )[0]
+        )[0]
     return None
