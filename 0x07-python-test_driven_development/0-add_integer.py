5162bcb088fa2d1dfc60bd7aa7a918e26e53e0d8
patrickdeyoreo <pdeyoreo@gmail.com>
2019-10-03 19:02:04 -0700

    Fixed function name in 0x07 task 0

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,8 +1,8 @@
 #!/usr/bin/python3
-"""Provides a function 'add' that adds two numbers"""
+"""Provides a function 'add_integer' that performs integer addition"""
 
 
-def add(a, b=98):
+def add_integer(a, b=98):
     """Add two integers"""
     if not isinstance(a, (float, int)):
         raise TypeError("a must be an integer")
