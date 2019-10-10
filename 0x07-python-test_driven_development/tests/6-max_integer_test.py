2c83fbac6da2f8095ae84973d6ea04c93fc9e500
patrickdeyoreo <pdeyoreo@gmail.com>
2019-10-09 23:49:41 -0700

    Added one-element list test in 0x07 task 5

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -24,25 +24,27 @@ class TestMaxInteger(unittest.TestCase):
         self.assertRaises(TypeError, max_integer, None)
 
     def test_BadListArguments(self):
-        """Test with a iterable non-list arguments"""
+        """Test with non-list or list-of-non-int iterable arguments"""
         self.assertEqual(max_integer('ABC'), 'C')
         self.assertEqual(max_integer([[], [0], [0, 0]]), [0, 0])
         self.assertRaises(TypeError, max_integer, [0, []])
 
     def test_EmptyListArguments(self):
-        """Test with a non-iterable argument"""
+        """Test with an empty list"""
         self.assertIs(max_integer([]), None)
 
-    def test_MaxMinListArguments(self):
+    def test_OneElementListArguments(self):
+        """Test with a list containing a single element"""
+        self.assertEqual(max_integer([0]), 0)
+
+    def test_LimitsListArguments(self):
+        """Test with lists containg limits (i.e. inf, -inf, nan)"""
         self.assertEqual(max_integer([0, float("inf")]), float("inf"))
         self.assertEqual(max_integer([0, float("-inf")]), 0)
         self.assertEqual(max_integer([float("inf"), 1]), float("inf"))
         self.assertEqual(max_integer([float("-inf"), 1]), 1)
         self.assertEqual(max_integer([0, float("inf"), 1]), float("inf"))
         self.assertEqual(max_integer([0, float("-inf"), 1]), 1)
-
-    def test_NanListArguments(self):
-        """Test with a non-iterable argument"""
         self.assertEqual(max_integer([0, float("nan")]), 0)
         self.assertNotEqual(max_integer([float("nan"), 1]), 1)
         self.assertEqual(max_integer([0, float("nan"), 1]), 1)
