c3bcb733a7bb473c24b6b80c204363812d31dafb
patrickdeyoreo <pdeyoreo@gmail.com>
2019-10-17 00:05:51 -0700

    adding pascal triangle solution

--- 0x0B-python-input_output/14-pascal_triangle.py
+++ 0x0B-python-input_output/14-pascal_triangle.py
@@ -5,4 +5,12 @@
 def pascal_triangle(n):
     """Compute the pascal triangle of n"""
     triangle = []
+    for i in range(n):
+        row = []
+        for j in range(i + 1):
+            if 0 < j < i:
+                row.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+            else:
+                row.append(1)
+        triangle.append(row)
     return triangle
