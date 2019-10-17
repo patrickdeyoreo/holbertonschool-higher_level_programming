efcb5286c70e924b9446bf322299898d505c127b
patrickdeyoreo <pdeyoreo@gmail.com>
2019-10-17 00:21:31 -0700

    Removed JSONDecodeError from 9-add_item.py

--- 0x0B-python-input_output/9-add_item.py
+++ 0x0B-python-input_output/9-add_item.py
@@ -2,7 +2,6 @@
 """Add all arguments to a list and save its JSON encoding to a file"""
 
 from sys import argv
-from json.decoder import JSONDecodeError
 
 save_to_json_file = __import__('7-save_to_json_file').save_to_json_file
 load_from_json_file = __import__('8-load_from_json_file').load_from_json_file
