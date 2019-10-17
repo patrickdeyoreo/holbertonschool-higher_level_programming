3702cbfc18e6d83edc250a183bff295a024033de
patrickdeyoreo <pdeyoreo@gmail.com>
2019-10-17 14:45:32 -0700

    Catching ValueError in Square

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -15,4 +15,6 @@ class Square(Rectangle):
             super().__init__(size, size)
         except TypeError:
             raise TypeError("size must be an integer")
+        except ValueError:
+            raise ValueError("size must be greater than 0")
         self.__size = size
