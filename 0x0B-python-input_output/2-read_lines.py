dcd8ac545b7d64db5316d46b7c9ddef64ca6b0b3
patrickdeyoreo <pdeyoreo@gmail.com>
2019-10-17 00:34:04 -0700

    Reading minimum amount of file in 2-read_lines.py

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -5,6 +5,13 @@
 def read_lines(filename="", nb_lines=0):
     """Print the specified number of lines of a file"""
     with open(filename, 'r') as istream:
-        lines = istream.readlines()
-        print(*lines[:nb_lines if nb_lines > 0 else len(lines)],
-              sep="\n", end="")
+        if nb_lines < 1:
+            print(istream.read(), end="")
+        else:
+            while nb_lines > 0:
+                line = istream.readline()
+                if line:
+                    print(line, end="")
+                    nb_lines -= 1
+                else:
+                    break
