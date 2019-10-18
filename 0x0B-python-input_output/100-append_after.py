bbc37f335d4d88ff162210aa8350262ab79a0f3c
patrickdeyoreo <pdeyoreo@gmail.com>
2019-10-17 23:31:43 -0700

    Not inserting addition newline in 100-append_after.py

--- 0x0B-python-input_output/100-append_after.py
+++ 0x0B-python-input_output/100-append_after.py
@@ -5,9 +5,9 @@
 def append_after(filename="", search_string="", new_string=""):
     """Insert text in a file after lines containing a specific string"""
     with open(filename, 'r+') as iostream:
-        lines = iostream.readlines()
+        lines = [
+            line + new_string * (search_string in line)
+            for line in iostream.readlines()
+        ]
         iostream.seek(0)
-        for line in lines:
-            iostream.write(line)
-            if search_string in line:
-                iostream.write(new_string + '\n')
+        iostream.writelines(lines)
