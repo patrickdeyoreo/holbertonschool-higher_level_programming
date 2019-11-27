06bd1cc2e2d67801918c947e64c9905a46cc0288
patrickdeyoreo <pdeyoreo@gmail.com>
2019-11-26 18:59:10 -0800

    Using inner join instead of left join

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- list all shows without a genre linked
 SELECT tv_genres.name AS genre, COUNT(*) as number_of_shows
-FROM tv_show_genres
-LEFT JOIN tv_genres
-ON tv_genres.id = tv_show_genres.genre_id
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_show_genres.genre_id = tv_genres.id
 GROUP BY tv_genres.name
 ORDER BY number_of_shows DESC;
