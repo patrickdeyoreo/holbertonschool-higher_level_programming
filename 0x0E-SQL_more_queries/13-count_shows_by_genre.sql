17d4c8ebe1d5db6d8b0be04430a6d3d964ca66dc
patrickdeyoreo <pdeyoreo@gmail.com>
2019-11-26 17:52:15 -0800

    Adding task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,8 +1,7 @@
 -- list all shows
-SELECT tg.name as genre, COUNT(tsg.show_id) as number_of_shows
+SELECT tg.name AS genre, COUNT(tsg.show_id) as number_of_shows
 FROM tv_genres tg
-INNER tv_show_genres tsg
+INNER JOIN tv_show_genres tsg
 ON tg.id = tsg.genre_id
-WHERE number_of_shows > 0
 GROUP BY genre
 ORDER BY number_of_shows DESC;
