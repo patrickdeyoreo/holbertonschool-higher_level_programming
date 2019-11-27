4cb8abac88da51ff6c0672eaf982577526c3e42b
patrickdeyoreo <pdeyoreo@gmail.com>
2019-11-26 19:02:05 -0800

    Trying incorrect output

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -3,5 +3,5 @@ SELECT tv_genres.name AS genre, COUNT(*) as number_of_shows
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_show_genres.genre_id = tv_genres.id
-GROUP BY tv_genres.name
+GROUP BY tv_show_genres.genre_id
 ORDER BY number_of_shows DESC;
