a4aeb4da70a0305c04dbdf29efe1a4e4921f2139
patrickdeyoreo <pdeyoreo@gmail.com>
2019-11-26 17:59:23 -0800

    Left join instead of Inner join on task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- list all shows
 SELECT tg.name AS genre, COUNT(tsg.show_id) as number_of_shows
 FROM tv_genres tg
-INNER JOIN tv_show_genres tsg
+LEFT JOIN tv_show_genres tsg
 ON tg.id = tsg.genre_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
