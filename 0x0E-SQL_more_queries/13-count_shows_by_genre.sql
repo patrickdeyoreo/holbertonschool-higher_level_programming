5d96b207fdcf8586b29ee80b7e24b71177bd47e5
patrickdeyoreo <pdeyoreo@gmail.com>
2019-11-26 18:53:26 -0800

    changed task 13

--- 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
+++ 0x0E-SQL_more_queries/13-count_shows_by_genre.sql
@@ -1,7 +1,7 @@
 -- list all shows without a genre linked
-SELECT tg.name AS genre, COUNT(*) as number_of_shows
-FROM tv_show_genres tsg
-LEFT JOIN tv_genres tg
-ON tg.id = tsg.genre_id
+SELECT tv_genres.name AS genre, COUNT(*) as number_of_shows
+FROM tv_show_genres
+LEFT JOIN tv_genres
+ON tv_genres.id = tv_show_genres.genre_id
 GROUP BY genre
 ORDER BY number_of_shows DESC;
