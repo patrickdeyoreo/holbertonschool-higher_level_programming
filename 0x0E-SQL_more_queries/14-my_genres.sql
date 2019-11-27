a8b3de7600b109d48c6d8c1a4e4ff2a232fe6dc7
patrickdeyoreo <pdeyoreo@gmail.com>
2019-11-26 19:24:31 -0800

    using left join instead of inner join in first

--- 0x0E-SQL_more_queries/14-my_genres.sql
+++ 0x0E-SQL_more_queries/14-my_genres.sql
@@ -1,10 +1,9 @@
 -- list all genres of the show Dexter
 SELECT tg.name
 FROM tv_genres tg
-INNER JOIN tv_show_genres tsg
+LEFT JOIN tv_show_genres tsg
 ON tg.id = tsg.genre_id
 INNER JOIN tv_shows ts
 ON ts.id = tsg.show_id
 WHERE ts.title = "DEXTER"
 ORDER BY title;
-
