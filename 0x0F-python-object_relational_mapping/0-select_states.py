86478724706833950a388836a9e511d3cde32eb8
patrickdeyoreo <pdeyoreo@gmail.com>
2019-12-15 15:01:43 -0800

    Added mysqlman module and task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,12 +1,24 @@
 #!/usr/bin/python3
 """
 List all states from a MySQL database localhost at port 3306
-Usage: 0-select_states.py username password database
 """
 
-import MySQLdb
-from sys import argv
+from mysqlman import MySQLMan
+from sys import argv, exit, stderr
 
 
 if __name__ == '__main__':
-    pass
+    try:
+        params = {
+            'user': argv[1],
+            'password': argv[2],
+            'database': argv[3],
+            'host': 'localhost',
+            'port': 3306,
+        }
+    except IndexError:
+        stderr.write('Usage: {} username password database\n'.format(argv[0]))
+        exit(1)
+    mysqlman = MySQLMan(connect=True, **params)
+    for row in mysqlman.query("SELECT id, name FROM states ORDER BY id;"):
+        print(row)
