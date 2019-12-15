9fc9f4e73d0ea70d4a9d02b8d5c17e98a89b1ba9
patrickdeyoreo <pdeyoreo@gmail.com>
2019-12-15 13:07:58 -0800

    Adding 0x0F project directory

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+List all states from a MySQL database localhost at port 3306
+Usage: 0-select_states.py username password database
+"""
+
+import MySQLdb
+from sys import argv
+
+
+if __name__ == '__main__':
+    pass
