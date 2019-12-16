27f7849c4d43884a126c88932b841daf2b997fee
patrickdeyoreo <pdeyoreo@gmail.com>
2019-12-16 08:31:08 -0800

    Reverted task 2 to have error handling

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -4,21 +4,39 @@ List all states matching given name from a MySQL db on localhost at port 3306
 """
 
 from mysqlman import MySQLMan
-from sys import argv
+from MySQLdb import Error
+from sys import argv, exit, stderr
+
+
+HELP = '{} username password database search'.format(argv[0])
+HOST = 'localhost'
+PORT = 3306
 
 
 if __name__ == '__main__':
-    params = {
-        'user': argv[1],
-        'password': argv[2],
-        'database': argv[3],
-        'host': 'localhost',
-        'port': 3306,
-    }
-    mysqlman = MySQLMan(connect=True, **params)
-    q = "SELECT id, name FROM states WHERE name = '{}' ORDER BY id;".format(
-        argv[4]
-    )
-    results = mysqlman.query([q, ()])[0]
-    for row in results:
+    try:
+        params = {
+            'user': argv[1],
+            'password': argv[2],
+            'database': argv[3],
+            'host': HOST,
+            'port': PORT,
+        }
+        search = argv[4]
+    except IndexError:
+        stderr.write('usage: {}\n'.format(HELP))
+        exit(2)
+    try:
+        mysqlman = MySQLMan(connect=True, **params)
+    except Error as e:
+        stderr.write('{}\n'.format(e.args[1]))
+        exit(1)
+    query = """
+    SELECT id, name
+    FROM states
+    WHERE name = '{}'
+    ORDER BY id;
+    """.format(search)
+    results = mysqlman.query([query, ()])
+    for row in results[0]:
         print(row)
