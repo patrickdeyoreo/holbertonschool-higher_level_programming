c5cf3ee16a339363d0c3b915f023e0078e7d6aad
Patrick DeYoreo <29614644+patrickdeyoreo@users.noreply.github.com>
2019-12-16 20:03:35 -0800

    Using city-state relationship to display cities

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -33,23 +33,9 @@ if __name__ == '__main__':
     Base.metadata.create_all(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
-    results = session.query(
-        State, City
-    ).filter(
-        State.id == City.state_id
-    ).order_by(
-        State.id, City.id
-    ).all()
-    cities_by_state = {}
-    for state, city in results:
-        s_repr = '{}: {}'.format(state.id, state.name)
-        c_repr = '{}: {}'.format(city.id, city.name)
-        if s_repr in cities_by_state:
-            cities_by_state[s_repr].append(c_repr)
-        else:
-            cities_by_state[s_repr] = [c_repr]
-    for s_repr in cities_by_state:
-        print(s_repr)
-        for c_repr in cities_by_state[s_repr]:
-            print('    ' + c_repr)
+    results = session.query(State).order_by(State.id)
+    for state in results.all()
+        print('{}: {}'.format(state.id, state.name))
+        for city in state.cities:
+            print('\t{}: {}'.format(city.id, city.name))
     session.close()
