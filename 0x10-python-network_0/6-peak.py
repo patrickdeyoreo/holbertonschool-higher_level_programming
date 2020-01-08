b8786c4991309fe9dd9d6affe2a3d1254a7ba38e
patrickdeyoreo <pdeyoreo@gmail.com>
2020-01-07 17:02:46 -0800

    Fixed magnitude comparison and slicing

--- 0x10-python-network_0/6-peak.py
+++ 0x10-python-network_0/6-peak.py
@@ -8,14 +8,15 @@ def find_peak(integers):
     """
     Finds a peak element in an unsorted list of integers
     """
-    if len(integers) == 0:
+    if not integers:
         return None
     if len(integers) == 1:
         return integers[0]
     if len(integers) == 2:
-        return integers[0] if integers[0] < integers[1] else integers[1]
-    if integers[len(integers) // 2] < integers[len(integers) // 2 - 1]:
-        return find_peak(integers[:len(integers) // 2])
-    if integers[len(integers) // 2] < integers[len(integers) // 2 + 1]:
-        return find_peak(integers[len(integers) // 2:])
-    return integers[len(integers) // 2]
+        return integers[0] if integers[0] > integers[1] else integers[1]
+    midpoint = len(integers) // 2
+    if integers[midpoint] < integers[midpoint - 1]:
+        return find_peak(integers[:midpoint])
+    if integers[midpoint] < integers[midpoint + 1]:
+        return find_peak(integers[midpoint + 1:])
+    return integers[midpoint]
