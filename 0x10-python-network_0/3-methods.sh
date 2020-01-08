7b8842b189bff4f28abf0ae99ccbd21dd490e96a
patrickdeyoreo <pdeyoreo@gmail.com>
2020-01-07 18:21:55 -0800

    Added '-' after cut field number to get the rest

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Display all HTTP methods the server of a URL will accept
-curl -sLI -- "$1" | grep -i 'allow' | tr -s '\t ' ' ' | cut -d ' ' -f 2
+curl -sLI -- "$1" | grep -i 'allow' | tr -s '\t ' ' ' | cut -d ' ' -f 2-
