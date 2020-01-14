d71270fc51e676086cc813c67ca6deb8a62a94ab
patrickdeyoreo <pdeyoreo@gmail.com>
2020-01-14 01:21:02 -0800

    Reverted task 13 to previous version

--- 0x11-python-network_1/102-starwars.py
+++ 0x11-python-network_1/102-starwars.py
@@ -11,15 +11,14 @@ URL = 'https://swapi.co/api/people'
 if __name__ == '__main__':
 
     params = {'search': sys.argv[1]}
-    search = requests.get(URL, params=params).json()
-    number = search.get('count')
-    people = search.get('results')
-    print('Number of results:', number)
-    while search.get('next'):
-        search = requests.get(search['next']).json()
-        people += search.get('results')
-    for person in people:
-        print(person.get('name'))
-        for link in person.get('films'):
-            film = requests.get(link).json()
-            print('\t', film.get('title'), sep='')
+    people = requests.get(URL, params=params).json()
+    print('Number of results:', people.get('count'))
+    while people:
+        for person in people.get('results'):
+            print(person.get('name'))
+            for link in person.get('films'):
+                print('\t{}'.format(requests.get(link).json().get('title')))
+        if people.get('next'):
+            people = requests.get(people.get('next')).json()
+        else:
+            people = None
