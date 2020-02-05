40f362ca044f3f951406c5f80bd45415b16e6249
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-05 00:57:10 -0800

    Fixed exported name in task 11

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 // Define a function to add two integers
-exports.add = function (x, theFunction) {
+exports.callMeMoby = function (x, theFunction) {
   for (let i = 0; i < x; i += 1) {
     theFunction();
   }
