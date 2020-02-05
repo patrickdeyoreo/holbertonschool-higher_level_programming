6625f8e8c4a524202b2ba0dc1ef9841b84f0a2c3
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-05 01:09:24 -0800

    Adding revised task 3 to meet requirements

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-// Print the first argument if any are provided
-if (process.argv.length > 2) {
+// Print the first argument if provided
+if (process.argv[2] !== undefined) {
   console.log(process.argv[2]);
 } else {
   console.log('No argument');
