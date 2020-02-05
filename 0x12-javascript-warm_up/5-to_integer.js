45fb5a064d0bed573d873fa6e6ab8e0797a7ac30
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-04 23:51:27 -0800

    Removed extra space from task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -4,5 +4,5 @@ const number = parseInt(process.argv[2]);
 if (Number.isNaN(number)) {
   console.log('Not a number');
 } else {
-  console.log(`My number:  ${number}`);
+  console.log(`My number: ${number}`);
 }
