c9351b94a97983d6a389e287e66415df6dc1e91c
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-05 00:39:07 -0800

    Adding task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,1 +1,8 @@
 #!/usr/bin/node
+// Searches for the second biggest integer in a list of arguments
+const numbers = process.argv.slice(2).map(x => parseInt(x));
+if (numbers.length < 2) {
+  console.log(0);
+} else {
+  console.log([...new Set(numbers)].sort((a, b) => b - a)[1]);
+}
