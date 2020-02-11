ac1cdca593734e13a5be1b8cccc1f1721378171b
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-10 21:01:26 -0800

    Adding task 5

--- 0x13-javascript_objects_scopes_closures/5-square.js
+++ 0x13-javascript_objects_scopes_closures/5-square.js
@@ -1,27 +1,14 @@
 #!/usr/bin/node
-// Definition of a rectangle
-module.exports = class Rectangle {
-  constructor (w, h) {
-    if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
-      this.width = w;
-      this.height = h;
-    }
+// Definition of a square (a square is a rectangle)
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
   }
 
-  print () {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
+  speak() {
+    console.log(`${this.name} barks.`);
   }
+}
 
-  rotate () {
-    this.width ^= this.height;
-    this.height ^= this.width;
-    this.width ^= this.height;
-  }
-
-  double () {
-    this.width *= 2;
-    this.height *= 2;
-  }
-};
