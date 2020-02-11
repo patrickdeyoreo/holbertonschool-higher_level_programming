31dfb26fd538ff0634279b6190ecfe760a59f8e3
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-10 22:05:07 -0800

    Handled error

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -1,27 +1,34 @@
 #!/usr/bin/node
 // Concatenate two files
 const fs = require('fs');
-fs.open(process.argv[4], 'w', (err, fd) => {
-  fs.readFile(process.argv[2], (err, data) => {
+try {
+  fs.open(process.argv[4], 'w', (err, fd) => {
     if (err) {
       throw err;
     }
-    fs.write(fd, data, (err, bytesWritten, buffer) => {
+    fs.readFile(process.argv[2], (err, data) => {
       if (err) {
         throw err;
       }
-      return bytesWritten;
+      fs.write(fd, data, (err, bytesWritten, buffer) => {
+        if (err) {
+          throw err;
+        }
+        return bytesWritten;
+      });
     });
-  });
-  fs.readFile(process.argv[3], (err, data) => {
-    if (err) {
-      throw err;
-    }
-    fs.write(fd, data, (err, bytesWritten, buffer) => {
+    fs.readFile(process.argv[3], (err, data) => {
       if (err) {
         throw err;
       }
-      return bytesWritten;
+      fs.write(fd, data, (err, bytesWritten, buffer) => {
+        if (err) {
+          throw err;
+        }
+        return bytesWritten;
+      });
     });
   });
-});
+} catch (err) {
+  console.log(err);
+}
