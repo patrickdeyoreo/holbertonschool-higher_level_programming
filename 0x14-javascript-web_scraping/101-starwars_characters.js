faa8f9f07a4b2cbcc37ed72c3f1a92022503f27b
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-12 03:21:49 -0800

    Reverting task 8 to original submission

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,20 +1,24 @@
 #!/usr/bin/node
 // Print all characters of a Star Wars movie
 const request = require('request');
-const url = `https://swapi.co/api/films/${process.argv[2]}`;
+const url = 'https://swapi.co/api/films';
 request(url, { json: true }, (err, resp, body) => {
   if (err) {
     console.log(err);
   } else if (body) {
-    const characters = body.characters;
-    for (const i in characters) {
-      request(characters[i], { json: true }, (err, resp, body) => {
-        if (err) {
-          console.log(err);
-        } else if (body) {
-          console.log(body.name);
+    for (const i in body.results) {
+      if (body.results[i].url.endsWith(`/${process.argv[2]}/`)) {
+        const characters = body.results[i].characters;
+        for (const j in characters) {
+          request(characters[j], { json: true }, (err, resp, body) => {
+            if (err) {
+              console.log(err);
+            } else if (body) {
+              console.log(body.name);
+            }
+          });
         }
-      });
+      }
     }
   }
 });
