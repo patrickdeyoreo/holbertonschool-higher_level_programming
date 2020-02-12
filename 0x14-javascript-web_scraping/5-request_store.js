f9b4b5a8a426985c92135cf33988306eb8193881
Patrick DeYoreo <pdeyoreo@gmail.com>
2020-02-11 22:48:09 -0800

    Printing empty body for task 5

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -4,7 +4,7 @@ const request = require('request');
 request(process.argv[2], (err, resp, body) => {
   if (err) {
     console.log(err);
-  } else if (body) {
+  } else {
     const fs = require('fs');
     fs.writeFile(process.argv[3], body, 'utf-8', (err) => {
       if (err) {
